9d1b82c931aed3d441587a517b3d45b83ae193f3
Matt Wormley <mwormley@adobe.com>
2013-10-04 17:05:57 -0700

    simplified snapshot lookup, using aws-sdk filters and new tags

--- chef/cookbooks/mongodb/libraries/ec2.rb
+++ chef/cookbooks/mongodb/libraries/ec2.rb
@@ -20,71 +20,25 @@ class Chef::ResourceDefinitionList::MongoDB
           :access_key_id => key,
           :secret_access_key => secret_key).regions[region]
 
-    # This is an array of arrays.  snapshots[i] will have the sorted list of
-    # snapshots for volumes[i].  The snapshots are sorted by description which
-    # has the snapshot timestamp in it, so the snapshots will be sorted in
-    # ascending order of timestamp.
-    snapshots = []
-
-    # We put all of this logic in a memoize block so each fetch of a snapshot
+    # group our snapshots by date_marker, and while were doing this, filter out snapshots that are incomplete or for 
+    # other volumes. We put all of this logic in a memoize block so each fetch of a snapshot
     # attribute will not result in a network connection.
+    snapshots_by_marker = Hash.new {|h,k| h[k] = Array.new}
     AWS.memoize do
-      for volume_id in volumes
-        # Get the list of snapshots for the currenet volume.
-        vol_snapshots = ec2.snapshots.select { |s| s.volume_id == volume_id }
-        vol_snapshots.sort! { |x, y| x.description <=> y.description }
-        # Remove the incomplete snapshots.  This makes the assumption that
-        # later snapshots will not finish before earlier snapshots.
-        while not vol_snapshots.size == 0 and
-              vol_snapshots.last.status != :completed
-          vol_snapshots.pop
-        end
-        snapshots.push vol_snapshots
-      end
-      Chef::Log.info "Found the list of snapshots"
-        r = /Mongo RAID (Snaphsot|Snapshot) (.*) ([[:digit:]]+)/
-      match_found = false
-      # Now we need to find the last complete set of snapshots.  This is done 
-      # by parsing the timestamp from the description of the newest snapshot
-      # for each volume (snapshots[i].last) and seeing if they are all the same.
-      # If they aren't, we pop the snapshot with the newest timestamp and 
-      # try again.  If any of the volumes has no more snapshots, then we don't
-      # have a consistent snapshot, so we return nil.
-      while not match_found
-        if snapshots[0].size == 0
-          return nil
-        end
-        # Keep track of the index of the snapshot array that has the
-        # description which is lexicographically the greatest. This should
-        # also be the newest snapshot created.
-        largest = 0
-        date = r.match(snapshots[0].last.description)[2]
-        match_found = true
+      volume_filter = []
+      volumes.each { |v| volume_filter << "volume-id" << v }
 
-        # Iterate over the newest snapshot for other volumes to see if the date
-        # matches the first one.
-        for i in 1..(snapshots.size() - 1)
-          if snapshots[i].size == 0
-            return nil
-          end
-
-          if snapshots[largest].last.description < snapshots[i].last.description
-            largest = i
-          end
-
-          if r.match(snapshots[i].last.description)[2] != date
-            match_found = false
-          end
-        end
-
-        if not match_found
-          snapshots[largest].pop
-        end
-      end
-      if match_found
-        Chef::Log.info "Starting from #{snapshots[0].last.description}"
-        return snapshots.map { |list| list.pop.id }
+      ec2.snapshots.filter(*volume_filter).filter("status", "completed").each do |s|
+        snapshots_by_marker[s.tags['date_marker']] << s if s.tags['date_marker']
       end
     end
+
+    # we find the last one that has the right number of volumes and return it
+    snapshots_by_marker.keys.reverse_each do |date_marker|
+      snapshots = snapshots_by_marker[date_marker]
+      return snapshots.map { |s| s.id } if snapshots.size == volumes.size
+    end
+
+    return nil
   end
 end
