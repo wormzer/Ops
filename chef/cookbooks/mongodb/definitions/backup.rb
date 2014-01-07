891cdd46596adb29522c96fadb2359427430de82
Matt Wormley <mwormley@adobe.com>
2014-01-07 13:46:51 -0800

    using environment credentials if available

--- chef/cookbooks/mongodb/definitions/backup.rb
+++ chef/cookbooks/mongodb/definitions/backup.rb
@@ -1,5 +1,9 @@
 define :generate_raid_backups do
-  aws_creds = Chef::EncryptedDataBagItem.load("passwords", node[:backups][:aws_passwords])
+  if ENV['AWS_SECRET_ACCESS_KEY']
+    aws_creds = { "aws_access_key_id" => ENV['AWS_ACCESS_KEY'], "aws_secret_access_key" => ENV['AWS_SECRET_ACCESS_KEY'] }
+  else
+    aws_creds = Chef::EncryptedDataBagItem.load("passwords", node[:backups][:aws_passwords])
+  end
 
 #  volumes = node[:backups][:mongo_volumes].join(" ")
 
@@ -10,7 +14,7 @@ define :generate_raid_backups do
     mode "0755"
     variables("seckey" => aws_creds["aws_secret_access_key"],
               "awskey" => aws_creds["aws_access_key_id"],
-						  "snapshot_tags" => node[:backups][:snapshot_tags].collect { |k,v| "#{k}=#{v}" }.join(' ') )
+              "snapshot_tags" => node[:backups][:snapshot_tags].collect { |k,v| "#{k}=#{v}" }.join(' ') )
   end
 end
 
