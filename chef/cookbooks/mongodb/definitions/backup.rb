ba55fb1906869dbbea0ac26f1449c5dfca13f242
Matt Wormley <mwormley@adobe.com>
2014-04-01 16:01:45 -0700

    splitting out daily/hourly clean from cron

--- chef/cookbooks/mongodb/definitions/backup.rb
+++ chef/cookbooks/mongodb/definitions/backup.rb
@@ -5,26 +5,27 @@ define :generate_raid_backups do
     aws_creds = Chef::EncryptedDataBagItem.load("passwords", node[:backups][:aws_passwords])
   end
 
-#  volumes = node[:backups][:mongo_volumes].join(" ")
-
-  template "/usr/local/bin/raid_snapshot.sh" do
-    source "raid_snapshot.sh.erb"
-    owner "root"
-    group "root"
-    mode "0755"
-    variables("seckey" => aws_creds["aws_secret_access_key"],
-              "awskey" => aws_creds["aws_access_key_id"],
-              "snapshot_tags" => node[:backups][:snapshot_tags].collect { |k,v| "#{k}=#{v}" }.join(' ') )
+  %q{hourly daily}.each do |period|
+    template "/usr/local/bin/#{period}_snapshot.sh" do
+      source "#{period}_snapshot.sh.erb"
+      owner "root"
+      group "root"
+      mode "0755"
+      variables("seckey" => aws_creds["aws_secret_access_key"],
+                "awskey" => aws_creds["aws_access_key_id"],
+                "snapshot_tags" => node[:backups][:snapshot_tags].collect { |k,v| "#{k}=#{v}" }.join(' ') )
+    end
   end
 
-  template "/usr/local/bin/forever_mongo_snapshot.sh" do
-    source "forever_mongo_snapshot.sh.erb"
-    owner "root"
-    group "root"
-    mode "0755"
-    variables("seckey" => aws_creds["aws_secret_access_key"],
-              "awskey" => aws_creds["aws_access_key_id"],
-              "snapshot_tags" => node[:backups][:snapshot_tags].collect { |k,v| "#{k}=#{v}" }.join(' ') )
+  cron "hourly_snapshot" do
+    command = "bash /usr/local/bin/hourly_snapshot.sh"
+    minute "0"
+    hour "02-22/2"
+  end
+
+  cron "daily_snapshot" do
+    command "bash /usr/local/bin/daily_snapshot.sh"
+    minute "0"
+    hour "0"
   end
 end
-
